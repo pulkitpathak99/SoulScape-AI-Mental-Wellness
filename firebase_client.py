9c8b883b0b0ec92ba9838b6db056bb77ce91cecb
Pulkit Pathak <112758477+pulkitpathak99@users.noreply.github.com>
2025-02-03 20:34:02 +0530

    Update firebase_client.py

--- firebase_client.py
+++ firebase_client.py
@@ -1,10 +1,24 @@
+
+# Initialize Firebase Admin SDK
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate("cred.json")
+cred = credentials.Certificate({
+    "type": os.environ.get("FIREBASE_TYPE"),
+    "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+    "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL")
+})
 firebase_admin.initialize_app(cred)
 db = firestore.client()
+
 
 def log_user_activity(user_id, activity, mood, timestamp):
     """
